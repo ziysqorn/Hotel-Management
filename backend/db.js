1b85c40e630b9fd656efd4809cb783423d72b8ed
Khang Nguyen <khangnguyen@MacBook-Air-cua-Khang.local>
2024-04-12 10:28:03 +0700

    put back db.js

---
+++ backend/db.js
@@ -0,0 +1,35 @@
+import sql, { query } from "mssql";
+
+const config = {
+  server: "localhost",
+  port: 1433,
+  database: "hotel_management",
+  user: "SA",
+  password: "Password123",
+  options: {
+    encrypt: true, // Bật mã hóa
+    trustServerCertificate: true, // Tin tưởng chứng chỉ máy chủ
+  },
+};
+// test ignore stuff
+async function db(queryString, parameters = []) {
+  try {
+    // Kết nối đến cơ sở dữ liệu
+    await sql.connect(config);
+    console.log(`Execute 4 ${queryString}`);
+
+    // Thực thi truy vấn
+    const result = await sql.query(queryString, ...parameters);
+
+    // Đóng kết nối sau khi thực thi truy vấn
+    await sql.close();
+
+    // Trả về kết quả
+    return result.recordset;
+  } catch (err) {
+    console.error("Error executing query:", err);
+    throw err;
+  }
+}
+
+export default db;
