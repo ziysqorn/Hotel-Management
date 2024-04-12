9f220d3b0562f7397ced252e9c2233cb1c321245
Hung Dang <106718735+HungMika@users.noreply.github.com>
2024-04-12 20:21:35 +0700

    api called success

--- backend/config.js
+++ backend/config.js
@@ -1,11 +1,17 @@
 export const config = {
-  server: "localhost",
-    port: 1433,
-    database: "hotel_management",
-    user: "SA",
-    password: "Password123",
-    options: {
-      encrypt: true, // Bật mã hóa
-      trustServerCertificate: true, // Tin tưởng chứng chỉ máy chủ
-    },
-  };
+  server: "localhost\\SQLEXPRESS",
+  port: 1433,
+  database: "hotel_management",
+   user: "sa",
+   password: "Huda2206",
+  options: {
+    //encrypt: true, // Bật mã hóa
+    trustServerCertificate: true, // Tin tưởng chứng chỉ máy chủ
+    trustedConnection: true
+  },
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000
+  }
+};
--- backend/db.js
+++ backend/db.js
@@ -2,23 +2,24 @@ import sql from "mssql";
 /**
  * @type {sql.Config}
  */
-const config = {
-  server: "localhost\\SQLEXPRESS",
-  port: 1433,
-  database: "hotel_management",
-   user: "sa",
-   password: "Huda2206",
-  options: {
-    //encrypt: true, // Bật mã hóa
-    trustServerCertificate: true, // Tin tưởng chứng chỉ máy chủ
-    trustedConnection: true
-  },
-  pool: {
-    max: 10,
-    min: 0,
-    idleTimeoutMillis: 30000
-  }
-};
+import { config } from "./config.js"; 
+// const config = {
+//   server: "localhost\\SQLEXPRESS",
+//   port: 1433,
+//   database: "hotel_management",
+//    user: "sa",
+//    password: "Huda2206",
+//   options: {
+//     //encrypt: true, // Bật mã hóa
+//     trustServerCertificate: true, // Tin tưởng chứng chỉ máy chủ
+//     trustedConnection: true
+//   },
+//   pool: {
+//     max: 10,
+//     min: 0,
+//     idleTimeoutMillis: 30000
+//   }
+// };
 
 async function db(queryString, parameters = []) {
   try {
