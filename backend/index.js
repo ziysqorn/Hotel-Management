8690011a960ec7d47b3adb86d90ba479a1a19f80
ziysqorn <tranduyquan2003@gmail.com>
2024-04-12 17:50:18 +0700

    Modified customer's API main route to api/customer/

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use("/api/room", roomRoute);
-app.use("/customer", customerRoute);
+app.use("/api/customer", customerRoute);
 
 
 app.listen(3000, () => {
